37c9f69cf1d8768a2dfb0e39d060fc729e28e71d
Brian Manning <brian@xaoc.org>
2013-06-07 11:38:37 -0700

    Jenkins.pm: whitespace and formatting changes

--- lib/Net/Jenkins.pm
+++ lib/Net/Jenkins.pm
@@ -11,30 +11,34 @@ use methods;
 use URI;
 use JSON;
 
-has scheme => ( is => 'rw', isa => 'Str', default => 'http' );
+has scheme => (
+    is      => 'rw',
+    isa     => 'Str',
+    default => 'http',
+);
 
-has host => ( 
-            is => 'rw', 
-            isa => 'Str' , 
-            default => sub {  
-                return $ENV{JENKINS_HOST} ? $ENV{JENKINS_HOST} : 'localhost';
-            });
+has host => (
+    is      => 'rw',
+    isa     => 'Str',
+    default => sub {
+        return $ENV{JENKINS_HOST} ? $ENV{JENKINS_HOST} : 'localhost';
+    },
+);
 
-has port => ( is => 'rw', isa => 'Int' , 
-            default => sub {  
-                return $ENV{JENKINS_PORT} ? $ENV{JENKINS_PORT} : 8080;
-            });
+has port => (
+    is      => 'rw',
+    isa     => 'Int',
+    default => sub { return $ENV{JENKINS_PORT} ? $ENV{JENKINS_PORT} : 8080; },
+);
 
-has user_agent => ( is => 'rw' , default => sub { 
-    return LWP::UserAgent->new;
-});
+has user_agent => (
+    is      => 'rw' ,
+    default => sub { return LWP::UserAgent->new; },
+);
 
 method get_base_url {
-    return $self->scheme 
-                . '://' . $self->host 
-                . ':' . $self->port;
+    return $self->scheme . '://' . $self->host . ':' . $self->port;
 }
-
 
 method post_url ($uri,%args) {
     return $self->user_agent->post( $uri , \%args );
@@ -59,7 +63,8 @@ method mode {
 }
 
 method jobs {
-    return map { Net::Jenkins::Job->new( %$_ , api => $self ) } @{ $self->summary->{jobs} };
+    return map { Net::Jenkins::Job->new( %$_ , api => $self ) }
+        @{ $self->summary->{jobs} };
 }
 
 method use_security {
@@ -83,13 +88,21 @@ method restart {
 method create_job ($job_name,$xml) {
     my $uri = URI->new( $self->get_base_url . '/createItem' );
     $uri->query_form( name => $job_name );
-    my $response = $self->user_agent->post( $uri , "Content-Type" => "application/xml" , Content => $xml );
+    my $response = $self->user_agent->post(
+        $uri,
+        "Content-Type"  => "application/xml",
+        Content         => $xml
+    );
     return $response->code == 200;
 }
 
 method update_job ($job_name,$xml) {
     my $uri = URI->new( $self->job_url($job_name) . '/config.xml' );
-    my $response = $self->user_agent->post( $uri , "Content-Type" => "application/xml" , Content => $xml );
+    my $response = $self->user_agent->post(
+        $uri,
+        "Content-Type"  => "application/xml",
+        Content         => $xml
+    );
     return $response->code == 200;
 }
 
@@ -97,7 +110,11 @@ method copy_job ($job_name, $from_job_name) {
     my $uri = URI->new( $self->get_base_url . '/createItem' );
 
     # name=NEWJOBNAME&mode=copy&from=FROMJOBNAME
-    $uri->query_form( name => $job_name , from => $from_job_name , mode => 'copy' );
+    $uri->query_form(
+        name => $job_name,
+        from => $from_job_name,
+        mode => 'copy',
+    );
     my $response = $self->user_agent->post( $uri );
     return $response->code == 302 ? 1 : 0;
 }
@@ -122,15 +139,11 @@ method build_job ($job_name) {
     return $self->post_url( $uri )->code == 302 ? 1 : 0;
 }
 
-
 method get_job_details ($job_name) {
     return $self->get_json(
         $self->job_url($job_name) . '/api/json'
     );
 }
-
-
-
 
 method get_builds ($job_name) {
     my $config = $self->get_job_details($job_name);
@@ -142,10 +155,11 @@ method get_build_details ($job_name, $number) {
     return $self->get_json($uri);
 }
 
-
 # Which returns a FH::Handle
 method get_build_console_handle ($job_name, $number) {
-    my $uri = URI->new( $self->job_build_url($job_name,$number) . '/consoleText' );
+    my $uri = URI->new(
+        $self->job_build_url($job_name,$number) . '/consoleText'
+    );
 
     # http://localhost:8080/job/Phifty/1/consoleText
 
@@ -164,7 +178,8 @@ method get_build_console ($job_name, $number) {
     while (1) {
         my $buf;
         my $n = $s->read_entity_body($buf, 1024);
-        die "build console of $job_name #$number read failed: $!" unless defined $n;
+        die "build console of $job_name #$number read failed: $!"
+            unless defined $n;
         last unless $n;
         $body .= $buf;
     }
